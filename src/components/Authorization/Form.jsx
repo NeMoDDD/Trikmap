b6b6c482cbb78cc59451b2a69e6555a441458cec
belek <bekakurmankulov1400@gmail.com>
2023-05-13 01:06:05 +0600

    add error message of auth and add antd in auth

--- src/components/Authorization/Form.jsx
+++ src/components/Authorization/Form.jsx
@@ -1,13 +1,16 @@
-import {useForm} from "react-hook-form";
+import {useForm, Controller} from "react-hook-form";
 import "./style.css"
 import {useState} from "react";
+import {Button, Input} from "antd";
+import {EyeTwoTone, EyeInvisibleOutlined} from '@ant-design/icons';
+import {useAuth} from "./hooks/use-auth";
 
 const isValidEmail = email =>
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
         email
     );
 const Form = ({btnValue, handleClick}) => {
-    const {register, handleSubmit, reset, formState: {errors}} = useForm({
+    const { control, handleSubmit, reset, formState: {errors}} = useForm({
         mode: "onBlur",
     });
     const [email, setEmail] = useState("")
@@ -21,27 +24,58 @@ const Form = ({btnValue, handleClick}) => {
         }
         return isValid
     };
+    const {isAuth} = useAuth()
+    const [isAuthSubmit, setIsAuthSubmit] = useState(true)
     const onSubmit = () => {
         handleClick(email, password)
-        reset()
+        if (!isAuth) {
+            setIsAuthSubmit(false)
+        }
     };
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className={"register-form"}>
-            <input className="register-form__input" placeholder="Email" {...register("email", {
-                required: "Это поле обязательное!", validate: handleEmailValidation,
-                onChange: (e) => setEmail(e.target.value)
-            })} />
-            {errors.email && <span>{errors.email.message || "Неверный email"}</span>}
-            <input className="register-form__input" placeholder="Пароль" {...register("password", {
-                required: true, minLength: {
-                    value: 6,
-                    message: "Минимум 6 символов!"
-                },
-                onChange: (e) => setPassword(e.target.value)
-            })} />
-            {errors.password && <span>{errors.password.message || "Это поле обязательное!"}</span>}
-            <input className="register-form__btn" type="submit" value={btnValue}/>
-        </form>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} className={"register-form"}>
+                <Controller
+                    name="email"
+                    control={control}
+                    rules={{
+                        required: "Это поле обязательное!", validate: handleEmailValidation,
+                        onChange: (e) => setEmail(e.target.value)
+                    }}
+                    render={({field}) => <Input {...field}
+                                                placeholder="Email"
+                                                className="register-form__input"
+                    />}/>
+                {errors.email && <span>{errors.email.message || "Неверный email"}</span>}
+                <Controller
+                    name="password"
+                    control={control}
+                    rules={{
+                        required: true, minLength: {
+                            value: 6,
+                            message: "Минимум 6 символов!"
+                        }, onChange: (e) => setPassword(e.target.value)
+                    }}
+                    render={({field}) => <Input.Password {...field}
+                                                         iconRender={(visible) => (visible ? <EyeTwoTone/> :
+                                                             <EyeInvisibleOutlined/>)}
+                                                         placeholder="Пароль"
+                                                         className="register-form__input"
+                    />}
+                />
+                {errors.password && <span>{errors.password.message || "Это поле обязательное!"}</span>}
+                {!isAuthSubmit && <span>Неверный email или пароль</span>}
+                <Controller
+                    name="btn-submit"
+                    control={control}
+                    render={({field}) => <Button {...field}
+                                                 className="register-form__btn"
+                                                 type="primary"
+                                                 htmlType="submit"
+                    >{btnValue}</Button>}
+                />
+            </form>
+        </>
     );
 }
 
