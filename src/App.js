1a8d7b27ed2104a83421cce7e23ffe168caa6598
softboy17 <kojcubekovryskeldi@gmail.com>
2023-05-18 23:16:38 +0600

    Доделал footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
-import Header from './components/header/Header';
-
+import Header from './components/header/Header'; 
+import Content from './components/content/Content'
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div>
       <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
