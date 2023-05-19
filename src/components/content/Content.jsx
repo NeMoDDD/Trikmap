8bd2f86f9a7a4315bf955a168a7b9984ed80b9a0
softboy17 <kojcubekovryskeldi@gmail.com>
2023-05-20 01:36:12 +0600

    Сделал чудеса света и текст "о нас"

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
-import Welcome from './components/Welcome/Welcome'
-import Wonder from './components/Wonders/Wonder'
+import Welcome from './components/Welcome/Welcome';
+import About from './components/About/About';
+//импорты картинки
+import Wonder from './components/Wonders/Wonder';
+import Issyk from '../../assets/image 11.jpg';
+import Sary from '../../assets/image 13.jpg';
+import Soun from '../../assets/Сон-Куль-1 1.jpg';
+import Ala from '../../assets/image 7.jpg';
+import Fireplace from '../../assets/image 8.jpg';
+import Waterfall from '../../assets/image 16.jpg';
+import AlaArcha from '../../assets/image 6.jpg';
+import JetiOguz from '../../assets/image 19.jpg'
+
+
 
 export default function Content() {
+  const images = [
+    {imageUrl: Issyk, text: 'Озеро Иссык-Куль'}, 
+    {imageUrl: Sary, text: 'Озеро Сары-Челек'}, 
+    {imageUrl: Soun, text: 'Озеро Сон-Куль'}, 
+    {imageUrl: Ala, text: 'Озеро Ала-Куль'}, 
+    {imageUrl: Fireplace, text: 'Комлекс отдыха 12 коминов'}, 
+    {imageUrl: Waterfall, text: 'Водопад Слезы Барса'}, 
+    {imageUrl: AlaArcha, text: 'Ущелье Ала-Арча'}, 
+    {imageUrl: JetiOguz, text: 'Ущелье Джети-Огуз'}   
+  ];
+  const imageAlt = 'Изображение';
   return (
     <div>
       <Welcome/>
-      <Wonder/>
+      <Wonder images={images} imageAlt={imageAlt}/>
+      <About/>
     </div>
   )
 }
--- src/components/content/components/Wonders/Wonder.jsx
+++ src/components/content/components/Wonders/Wonder.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import s from '../Wonders/Wonder.module.css'
-export default function Wonder() {
+export default function Wonder({images, imageAlt}) {
+
   return (
     <div className={s.main}>
         <div className={s.title}>
             <h1>Чудеса Кыргызстана</h1>
         </div>
-        
-    </div>
+        <div className={s.img}>
+          {images.map((image, index)=>(
+            <div key={index}>
+            <img key={index} src={image.imageUrl} alt={`${imageAlt} ${index + 1}`} />
+            <p>{image.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
   )
 }
