2c0edcb2831dfd6a4f326f47615ea0eec284fd03
softboy17 <kojcubekovryskeldi@gmail.com>
2023-05-30 00:56:02 +0600

    сделал адаптивку для header и welcome

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,41 +1,38 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
 import classes from './Header.module.css'
-import Logo from '../../assets/img/Лого.svg'
-export default function Header() {
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/img/Лого.svg';
+
+const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div className={classes.main}>
-        <div className={classes.header}>
-            <div className={classes.logo}>
-                <img src={Logo} alt="logo" />
-                <h3>TRIKMAP</h3>
-            </div>
-            <div className={classes.link}>
-                <NavLink to="/">
-                    Главная
-                </NavLink>
-                <NavLink to="/">
-                    Отели
-                </NavLink>
-                <NavLink to="/">
-                    Туры
-                </NavLink>
-                <NavLink to="/">
-                    Достопримечательности
-                </NavLink>
-                <NavLink to="/">
-                    Личный кабинет
-                </NavLink>
-            </div>
-            <div className={classes.login}>
-                <NavLink to= '/'>Войти</NavLink>
-                <button>
-                    Регистрация
-                </button>
-            </div>
+      <div className={classes.header}>
+        <div className={classes.logo}>
+          <img src={Logo} alt="logo" />
+          <h3>TRIKMAP</h3>
         </div>
-        
+        <div className={`${classes.link} ${isMenuOpen ? classes.open : ''}`}>
+          <NavLink to="/">Главная</NavLink>
+          <NavLink to="/">Отели</NavLink>
+          <NavLink to="/">Туры</NavLink>
+          <NavLink to="/">Достопримечательности</NavLink>
+          <NavLink to="/">Личный кабинет</NavLink>
+        </div>
+        <div className={classes.mobileMenu} onClick={toggleMenu}>
+          <div className={isMenuOpen ? classes.burgerActive : classes.burger} />
+        </div>
+        <div className={`${classes.login} ${isMenuOpen ? classes.open : ''}`}>
+          <NavLink to="/">Войти</NavLink>
+          <button>Регистрация</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Header;
