739af1ca1da7f3178af78bb0697a1329d0d5708c
belek <bekakurmankulov1400@gmail.com>
2023-05-30 11:17:22 +0600

    finish fix select type

--- src/components/Attractions/Attractions.jsx
+++ src/components/Attractions/Attractions.jsx
@@ -19,22 +19,30 @@ const Attractions = () => {
         totalCountNaryn,
         totalCountOsh,
         totalCountTalas,
-        totalCountOfType,
+        // totalCountOfType,
     } = useSelector(state => state.attractions)
     const [totalCount, setTotalCount] = useState(totalCountAll)
     const [region, setRegion] = useState("all")
     const [dataRegion, setDataRegion] = useState(data.all)
-    const [type, setType] = useState("All")
+    const [type, setType] = useState("all")
+    const [totalCountOfType, setTotalCountOfType] = useState()
 
     const lastPostIndex = currentPage * pageSize;
     const firstPostIndex = lastPostIndex - pageSize
 
     const onSelectChange = (value) => {
-        if (value !== "All") {
-            setType(value)
-
-            onClickRegion(region, dataRegion.filter(p => p.properties.type === value).length)
+        setType(value)
+        if (value !== "all") {
+            setTotalCount(dataRegion.filter(p => p.properties.type === value).length)
+            setTotalCountOfType(dataRegion.filter(p => p.properties.type === value).length)
+        } else {
+            setTotalCount(dataRegion.length)
         }
+        dispatch(setCurrentPage({
+            currentPage: 1
+        }))
+        // onClickRegion(region, dataRegion.filter(p => p.properties.type === value).length, null)
+        // }
         setType(value)
     };
 
@@ -43,11 +51,11 @@ const Attractions = () => {
             currentPage: id
         }))
     }
-    const onClickRegion = (region, totalCount) => {
-        if (type !== "All") {
-            setTotalCount(totalCount)
+    const onClickRegion = (region, totalCount, totalCountOfRegion, data) => {
+        if (type === "all") {
+            setTotalCount(totalCountOfRegion)
         } else {
-            setTotalCount(totalCount)
+            setTotalCount(data.filter(p => p.properties.type === type).length)
         }
         dispatch(setCurrentPage({
             currentPage: 1
@@ -56,40 +64,40 @@ const Attractions = () => {
     }
 
     const onClickAllRegion = () => {
-        onClickRegion("all", totalCountAll)
         setDataRegion(data.all)
+        onClickRegion("all", null, totalCountAll, data.all)
     }
     const onClickChuyRegion = () => {
-        onClickRegion("chuy", totalCountChuy)
         setDataRegion(data.chuy)
+        onClickRegion("chuy", null, totalCountChuy, data.chuy)
     }
     const onClickTalasRegion = () => {
-        onClickRegion("talas", totalCountTalas)
         setDataRegion(data.talas)
+        onClickRegion("talas", null, totalCountTalas, data.talas)
     }
     const onClickOshRegion = () => {
-        onClickRegion("osh", totalCountOsh)
         setDataRegion(data.osh)
+        onClickRegion("osh", null, totalCountOsh, data.osh)
     }
     const onClickBatkenRegion = () => {
-        onClickRegion("batken", totalCountBatken)
         setDataRegion(data.batken)
+        onClickRegion("batken", null, totalCountBatken, data.batken)
     }
     const onClickJalalabadRegion = () => {
-        onClickRegion("jalalabad", totalCountJalabad)
         setDataRegion(data.jalalabad)
+        onClickRegion("jalalabad", null, totalCountJalabad, data.jalalabad)
     }
     const onClickNarynRegion = () => {
-        onClickRegion("naryn", totalCountNaryn)
         setDataRegion(data.naryn)
+        onClickRegion("naryn", null, totalCountNaryn, data.naryn)
     }
     const onClickIssykkolRegion = () => {
-        onClickRegion("issykkol", totalCountIssykkol)
         setDataRegion(data.issykkol)
+        onClickRegion("issykkol", null, totalCountIssykkol, data.issykkol)
     }
 
     const regionVisible = (regionOn, dataRegion) => {
-        return (region === regionOn && type !== "All" ? dataRegion.filter(p => p.properties.type === type).slice((firstPostIndex), lastPostIndex)
+        return (region === regionOn && type !== "all" ? dataRegion.filter(p => p.properties.type === type).slice((firstPostIndex), lastPostIndex)
             .map((p, index) => {
                 return (
                     <Attraction location={p.properties.location}
@@ -97,7 +105,7 @@ const Attractions = () => {
                                 title={p.properties.name}
                                 imgSrc={p.properties.image}
                                 key={index}/>)
-            }) : (region === regionOn && type === "All" ? dataRegion.slice(firstPostIndex, lastPostIndex).map((d, index) =>
+            }) : (region === regionOn && type === "all" ? dataRegion.slice(firstPostIndex, lastPostIndex).map((d, index) =>
             <Attraction location={d.properties.location}
                         description={d.properties.description}
                         title={d.properties.name}
@@ -128,7 +136,7 @@ const Attractions = () => {
         </div>
         <div className={style.selectBlock}>
             <Select
-                defaultValue="All"
+                defaultValue="all"
                 style={{
                     width: 120,
                 }}
@@ -136,7 +144,7 @@ const Attractions = () => {
 
                 options={[
                     {
-                        value: 'All',
+                        value: 'all',
                         label: 'Все',
                     },
                     {
