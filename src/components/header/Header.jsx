b7ce57db8d0d95bd281b21217d7d433fe04fab9b
softboy17 <kojcubekovryskeldi@gmail.com>
2023-06-01 23:33:46 +0600

    header fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classes from './Header.module.css'
 import { NavLink } from 'react-router-dom';
-import Logo from '../../assets/img/Лого.svg';
-
+import Logo from '../../assets/img/Лого.svg'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button
+} from '@chakra-ui/react'
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [hideOrShow, setHideOrShow] = useState({})
-
-  const handleMenu = () =>{
-    setIsOpen((prev) => !prev)
-    if(isOpen){
-      setHideOrShow(()=> {
-        return{}
-      })
-    }
-    else{
-      setHideOrShow(()=> {
-        return{ display:'flex' }
-      })
-    }
-  }
   return (
     <div className={classes.main}>
-      <div className={classes.header}>
+      <div div className={classes.header}>
         <div className={classes.logo}>
           <img src={Logo} alt="logo" />
           <h3>TRIKMAP</h3>
         </div>
-        
-        <div className={classes.link} style={hideOrShow}>
+        <div className={classes.link}>
           <NavLink to="/">Главная</NavLink>
           <NavLink to="/">Отели</NavLink>
           <NavLink to="/">Туры</NavLink>
           <NavLink to="/">Достопримечательности</NavLink>
           <NavLink to="/">Личный кабинет</NavLink>
         </div>
-        
+
         <div className={classes.login}>
           <NavLink to="/">Войти</NavLink>
           <button>Регистрация</button>
         </div>
-        {isOpen ? <div className={classes.burger} onClick={handleMenu}  >
-          <span></span>
-        </div> : <div className={classes.burger} onClick={handleMenu}  >
-          <span></span>
-        </div> }
+
+
+        <div className={classes.menu}>
+          <Menu className = {classes.menu_wrapper}>
+            <MenuButton as={Button}  className={classes.customMenuButton}>
+              Меню
+            </MenuButton >
+            <MenuList className={classes.customMenuList}>
+              <MenuItem >Главная</MenuItem>
+              <MenuItem >Отели</MenuItem>
+              <MenuItem>Туры</MenuItem>
+              <MenuItem>Достопримечательности</MenuItem>
+              <MenuItem>Личный кабинет</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </div>
     </div>
   );
