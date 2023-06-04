189d6ed4f22c6ad1228422890c7276fbe8ccd22d
Anixii <kenjebekoverbol53@gmail.com>
2023-06-05 00:12:33 +0600

    Test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-
-import { NavLink, Route, Routes } from 'react-router-dom';
-import './App.css'   
+import classes from './components/footer/Footer.module.css'
+import './App.css';
+import Header from './components/header/Header';  
+import Content from './components/content/Content'
+import Footer from './components/footer/Footer'; 
+import HotelContainer from './components/Hotel/HotelContainer' 
+import TourContainer from './components/Tours/TourContainer' 
+import { Route, Routes } from 'react-router'; 
 import { lazy } from 'react';
-import HotelContainer from './components/Hotel/HotelContainer'; 
-import AddhotelContainer from './components/Hotel/addHotel/addhotelContainer';
-import TourContainer from './components/Tours/TourContainer';  
-import Error from './components/common/Error';
-import FormOrderHotel from './components/Hotel/HotelInfo/OrderHotel/FormOrder/FormOrderHotel';
 const OrderHotelContainer = lazy(() =>import('./components/Hotel/HotelInfo/OrderHotel/OrderHotelContainer'))
-function App() {    
-  return(  
-    <div>      
-      <NavLink to={'/add'}> Add hotel</NavLink>
-      <NavLink to={'/hotels'}> Hotels</NavLink> 
-      <NavLink to={'/tours/'}> TOurs</NavLink> 
-      <NavLink to={'/order'}>Lox</NavLink>
+function App() {
+  return (
+    <div className={classes.container}>
+      <Header/> 
       <Routes>  
-        <Route path='/add/' element={<AddhotelContainer/>}/>
-        <Route path='/hotels/' element={<HotelContainer/>}>  </Route>  
-        <Route path='/hotels/:hotel' element={<OrderHotelContainer/>}/> 
-        <Route path='/tours/' element={<TourContainer/>}/> 
-        <Route path='/error/' element={<Error/>}/> 
-        <Route path='/order' element={<FormOrderHotel/>}/>
+      <Route path='/' element={<Content/>}/> 
+      <Route path='/hotels' element={<HotelContainer/>}/> 
+      <Route path='/tours' element={<TourContainer/>}/> 
+      <Route path='/hotels/:hotel' element={<OrderHotelContainer/>}/> 
       </Routes>
+      <Footer/>
     </div>
-    )
-  }
-  
-  export default App;
-  
+  );
+}
+
+export default App;
