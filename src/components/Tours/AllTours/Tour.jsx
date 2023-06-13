5039cd2ea98bd52274f5b81f714312420d4a1b72
Anixii <kenjebekoverbol53@gmail.com>
2023-06-13 16:03:39 +0600

    System of  rating for tours

--- src/components/Tours/AllTours/Tour.jsx
+++ src/components/Tours/AllTours/Tour.jsx
@@ -1,3 +1,4 @@
+import { Rate } from 'antd'
 import s from './Tours.module.css'    
 import { NavLink } from 'react-router-dom'
 const Tour = ({tour,...props}) =>{ 
@@ -6,7 +7,8 @@ const Tour = ({tour,...props}) =>{
 
         <div style={{backgroundImage: `url(${tour.photo[0]}), -webkit-linear-gradient(top, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.2))` }} className={s.tour__about}> 
             <div className={s.about__item}>   
-                <div className={s.about__title}>{tour.title}</div>  
+                <div className={s.about__title}>{tour.title}</div>   
+                <div className={s.about__rate}><Rate value={tour.rating}/></div>
                 <div className={s.about__date}> 
                     <ul> 
                         <li className={s.about__list}>Даты:</li> 
