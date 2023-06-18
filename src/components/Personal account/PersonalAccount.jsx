e005ef0e5880aea1173e0a4adaf111d6598fed1e
Anixii <kenjebekoverbol53@gmail.com>
2023-06-18 21:47:48 +0600

    fixed bugs

--- src/components/Personal account/PersonalAccount.jsx
+++ src/components/Personal account/PersonalAccount.jsx
@@ -95,6 +95,7 @@ const PersonalAccount = React.memo(({ getBookedTourTC, bookedHotel, getBookedHot
                                     {isFetching ? <div className={style.modal__loader}>
                                         <Spinner className={style.modal__spin} color='blue' colorScheme='cyan' />
                                     </div> : bookedHotel?.data ? bookedHotel.data.map((item, index) => <div key={index} className={style.modal__inner}>
+                                         
                                         <div className={style.modal__descrip}>{item.name}</div>
                                         <div className={style.modal__item}>
                                             <div className={style.modal__title}>Название Отеля:</div>
