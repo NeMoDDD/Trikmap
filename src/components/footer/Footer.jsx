2d4022a9c688b120297f36c2daa6d7e8149b15d3
Erbol Kenzhebekov <96565032+Anixii@users.noreply.github.com>
2025-03-24 21:14:12 +0600

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -30,11 +30,11 @@ export default function Footer({scrollToRef, ...props}) {
                             <h3>Follow</h3>
                         </div>
                         <div className={classes.icons}>
-                            <a href={"https://instagram.com/kurmankulovb_?igshid=MmIzYWVlNDQ5Yg=="} target="_blank"><img
+                            <a href={"https://instagram.com/anixii_"} target="_blank"><img
                                 src={Instagram} alt="instagram" className={classes.img}/></a>
-                            <a href={"https://t.me/belekkurm"} target="_blank"><img src={Telegram} alt="telegram"
+                            <a href={"https://t.me/anixii21"} target="_blank"><img src={Telegram} alt="telegram"
                                                                                     className={classes.img}/></a>
-                            <a href={"https://wa.me/+996553442209"} target="_blank"><img src={Whatsapp} alt="whatsapp"
+                            <a href={"https://wa.me/+996704712217"} target="_blank"><img src={Whatsapp} alt="whatsapp"
                                                                                          className={classes.img}/></a>
                         </div>
                     </div>
